25af267881e98e17643c150bc7fb8cb4f786a78b
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-16 12:57:10 -0500

    Added setExpenses to the localStorage.getItem() useEffect to make sure it runs if setExpense s were to ever change unexpectedly.

--- src/Comps/Feature-2/Feature-SortedCategory/StoredExpenses.js
+++ src/Comps/Feature-2/Feature-SortedCategory/StoredExpenses.js
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 
- export const StoredExpenses = ({ expenses, setExpenses }) => {
+export const StoredExpenses = ({ expenses, setExpenses }) => {
   // Only run once when this component is first rendered
-  
+
   useEffect(() => {
     if (localStorage.getItem("storedExpensesData")) {
-       
       console.log("Before parsing:", localStorage.getItem("storedExpensesData")); // Log before JSON.parse
       setExpenses(JSON.parse(localStorage.getItem("storedExpensesData")));
       console.log("After parsing:", JSON.parse(localStorage.getItem("storedExpensesData")));
     }
-  }, []);
+  }, [setExpenses]);
+
   // Run every time our expenses state changes
   useEffect(() => {
     const stringifiedExpenses = JSON.stringify(expenses);
@@ -20,7 +20,5 @@ import { useEffect } from "react";
 
   useEffect(() => {
     console.log("Updated expenses state:", expenses);
-  }, [expenses]);  // Runs every time expenses changes
-  
+  }, [expenses]); // Runs every time expenses changes
 };
-
