7273dc2823a395e7ebdb4f4652b05286d287cf33
Aguadebebe <81484212+Aguadebebe@users.noreply.github.com>
2025-02-18 21:34:30 -0500

    Added new tasks to App.js file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,6 @@
 import Header from "./Feature-1/Header";
 import Expense from "./Feature-2/Feature-SortedCategory/Expense";
 import SideBar from "./Feature3/SideBar";
-import "../Styles/App.css";
 
 function App() {
   return (
@@ -9,7 +8,7 @@ function App() {
       <div className="sidebar bg-light border-end">
         <SideBar />
       </div>
-      <div className=" flex-grow-1">
+      <div className="flex-grow-1">
         <Header />
         <Expense />
       </div>
@@ -18,3 +17,8 @@ function App() {
 }
 
 export default App;
+// Add file save file icon to form
+// set up server.js file with express
+// set up mongo db to store expense files
+// set up fetch rquest for file to be stored in frontend sidebar with screenshot icon
+//set up .get request to send/recive files from mongo db
